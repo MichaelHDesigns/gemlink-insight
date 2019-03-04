142fdbcfbbb1be241d59554848adb97e3c903ad7
Snowgem <34765060+Snowgem@users.noreply.github.com>
2019-03-04 12:38:44 +0700

    Update explorer.sh

--- explorer.sh
+++ explorer.sh
@@ -16,6 +16,13 @@ sudo n 4
 # install ZeroMQ libraries
 sudo apt-get -y install libzmq3-dev
 
+# install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+
 # install snowgem version of bitcore
 npm install snowgem/bitcore-node-snowgem
 
