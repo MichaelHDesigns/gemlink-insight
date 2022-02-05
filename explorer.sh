ecc4681f2f16908ee61156b72d26acad09d88337
devxz <tinh.mrlov3@gmail.com>
2022-02-05 09:39:22 +0700

    Update explorer.sh

--- explorer.sh
+++ explorer.sh
@@ -25,15 +25,11 @@ cd ..
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
-# install ZeroMQ libraries
-sudo apt-get -y install libzmq3-dev
 
 # install mongodb
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo service mongod start
+sudo apt-get install -y mongodb
+sudo service mongodb start
 
 # install Gemlink version of bitcore
 npm install gemlink/bitcore-node-gemlink
